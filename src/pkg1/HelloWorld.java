e7f3c8f8357a37cae492a9039520472aa8a817ea
himanshucsi <himanshucsi.suman@gmail.com>
2021-10-19 10:19:06 +0530

    adding files

---
+++ src/pkg1/HelloWorld.java
@@ -0,0 +1,14 @@
+package pkg1;
+
+public class HelloWorld {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		int myFirstNumber = 8;
+		char myFirstChar = 'H';
+		System.out.println(myFirstNumber + (char)myFirstChar);
+		System.out.println(myFirstChar + myFirstNumber);
+	}
+
+}
